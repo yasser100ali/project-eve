d5c3b0660289fe57c25cfbcbece7c23291a6c781
yasser100ali <108766553+yasser100ali@users.noreply.github.com>
2025-09-27 20:56:08 -0700

    updated drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,5 @@
 import { config } from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
-import { neon } from '@neondatabase/serverless';
 
 config(); // Loads .env.local for CLI
 
@@ -8,5 +7,7 @@ export default defineConfig({
   schema: './lib/db/schema.ts',
   out: './lib/db/migrations',
   dialect: 'postgresql',
-  dbCredentials: neon(process.env.DATABASE_URL!),
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
 });
